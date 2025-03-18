550cf962866dd99f18fada51f5a5a8f318f32158
jm4293 <dlwoals4293@gmail.com>
2025-03-18 23:04:42 +0900

    [Design]: layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,18 @@
+import Header from '@/app/(main)/_components/header';
+import Navbar from '@/app/(main)/_components/navbar';
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function Layout(props: IProps) {
+  const { children } = props;
+
+  return (
+    <div className="layout">
+      <Header />
+      <div className="content">{children}</div>
+      <Navbar />
+    </div>
+  );
+}
