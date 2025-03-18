ce8948f18b78dd7615725aef74f5b5e87e6e6216
jm4293 <dlwoals4293@gmail.com>
2025-03-19 00:21:46 +0900

    [Chore]: header

--- app/(main)/_components/header.tsx
+++ app/(main)/_components/header.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+const headerPathName: { [key: string]: string } = {
+  '/home': '홈',
+  '/stock': '주식',
+  '/board': '게시판',
+  '/board/detail': '게시판 작성',
+  '/mypage': '마이페이지',
+  '/mypage/board': '작성한 게시글',
+  '/mypage/comment': '작성한 댓글',
+  '/notification': '알림',
+};
+
 export default function Header() {
-  return <div className="header">header</div>;
+  const pathname = usePathname();
+
+  return (
+    <div className="header">
+      <div>
+        <p>뒤로가기</p>
+      </div>
+      <div>
+        <p>{headerPathName[pathname]}</p>
+      </div>
+      <div>
+        <p>알림</p>
+      </div>
+    </div>
+  );
 }
--- hooks/useDeviceLayout.tsx
+++ hooks/useDeviceLayout.tsx
@@ -1,13 +1,13 @@
+'use client';
+
 import { useEffect, useState } from 'react';
 
 export const useDeviceLayout = () => {
   const [width, setWidth] = useState<number>(window.innerWidth);
-  const [height, setHeight] = useState<number>(window.innerHeight);
 
   useEffect(() => {
     const handleResize = () => {
       setWidth(window.innerWidth);
-      setHeight(window.innerHeight);
     };
 
     window.addEventListener('resize', handleResize);
