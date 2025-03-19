d65471a4828b317f0d4ec8371ad4085e45bbe8df
jm4293 <dlwoals4293@gmail.com>
2025-03-19 21:32:23 +0900

    [Build]: tailwind.config에서 content 경로 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './asset/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {},
   },
