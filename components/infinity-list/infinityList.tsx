891350904b8549148d539f224a702a51666f0e45
jm4293 <dlwoals4293@gmail.com>
2025-03-23 14:35:37 +0900

    [Refactor]: infinityList 컴포넌트 구조 변경

--- components/infinity-list/infinityList.tsx
+++ components/infinity-list/infinityList.tsx
@@ -1,16 +1,20 @@
+'use client';
+
 import { JSX, useEffect, useRef } from 'react';
 import Skeleton from '@/components/skeleton/skeleton';
+import Text from '@/components/text/text';
 
 interface IProps<T> {
-  data: T[];
+  data: T[] | undefined;
   renderItem: (item: T) => JSX.Element | JSX.Element[];
+  total: number | undefined;
   fetchNextPage: () => void;
   hasNextPage: boolean;
   isFetchingNextPage: boolean;
 }
 
 export default function InfinityList<T>(props: IProps<T>) {
-  const { data, renderItem, fetchNextPage, hasNextPage, isFetchingNextPage } = props;
+  const { data = [], renderItem, total = 0, fetchNextPage, hasNextPage, isFetchingNextPage } = props;
 
   const observerRef = useRef<IntersectionObserver | null>(null);
   const loadMoreRef = useRef<HTMLDivElement | null>(null);
@@ -39,11 +43,17 @@ export default function InfinityList<T>(props: IProps<T>) {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="w-full flex flex-col gap-8">{data.map(renderItem)}</div>
+      {total > 0 ? (
+        <>
+          <div className="w-full flex flex-col gap-8">{data.map(renderItem)}</div>
 
-      <div ref={loadMoreRef} className="flex justify-center items-center">
-        {isFetchingNextPage && <Skeleton />}
-      </div>
+          <div ref={loadMoreRef} className="flex justify-center items-center">
+            {isFetchingNextPage && <Skeleton />}
+          </div>
+        </>
+      ) : (
+        <Text value="없습니다." color="#000000" />
+      )}
     </div>
   );
 }
--- components/infinity-list/infinityListWrapper.tsx
+++ components/infinity-list/infinityListWrapper.tsx
@@ -1,27 +1,21 @@
+'use client';
+
 import { JSX } from 'react';
 import Text from '@/components/text/text';
 
 interface IProps {
   total: number | undefined;
-  renderHeader?: JSX.Element;
-  renderList?: JSX.Element;
+  renderList: JSX.Element;
 }
 
 export default function InfinityListWrapper(props: IProps) {
-  const { total = 0, renderHeader, renderList } = props;
+  const { total = 0, renderList } = props;
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-1">
-          <Text value="총" color="#000000" size="lg" />
-          <div className="flex">
-            <Text value={String(total)} color="#000000" />
-            <Text value="개" color="#000000" />
-          </div>
-        </div>
-
-        {renderHeader && <div>{renderHeader}</div>}
+    <div className="flex flex-col gap-8">
+      <div className="flex items-center gap-1">
+        <Text value="총" color="#000000" size="lg" />
+        <Text value={`${String(total)}개`} color="#000000" />
       </div>
 
       <div>{renderList}</div>
