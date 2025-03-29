ba9c9606fe3951fb607f2167466c7a0660764b0d
jm4293 <dlwoals4293@gmail.com>
2025-03-29 23:14:10 +0900

    [Style]: header title

--- app/(main)/_components/headerTitle.tsx
+++ app/(main)/_components/headerTitle.tsx
@@ -1,21 +1,40 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+
+// const headerPathName: { [key: string]: string } = {
+//   '/home': '홈',
+//   '/home/guest': '홈',
+//   '/stock': '주식',
+//   '/stock/guest': '주식',
+//   '/board': '게시판',
+//   '/board/guest': '게시판',
+//   '/board/register': '게시판 등록',
+//   '/board/detail': '게시판 작성',
+//   '/user': '내 정보',
+//   '/user/guest': '내 정보',
+//   '/user/board': '작성한 게시글',
+//   '/user/comment': '작성한 댓글',
+//   '/notification': '알림',
+// };
 
 const headerPathName: { [key: string]: string } = {
-  '/home': '홈',
-  '/stock': '주식',
-  '/board': '게시판',
-  '/board/register': '게시판 등록',
-  '/board/detail': '게시판 작성',
-  '/user': '내 정보',
-  '/user/board': '작성한 게시글',
-  '/user/comment': '작성한 댓글',
-  '/notification': '알림',
+  home: '홈',
+  stock: '주식',
+  board: '게시판',
+  user: '내 정보',
+  notification: '알림',
 };
 
 export default function HeaderTitle() {
   const pathname = usePathname();
 
-  return <p className="text-center">{headerPathName[pathname]}</p>;
+  const title = useMemo(() => {
+    const path = pathname.split('/')[1];
+
+    return headerPathName[path];
+  }, [pathname]);
+
+  return <p className="text-center">{title}</p>;
 }
