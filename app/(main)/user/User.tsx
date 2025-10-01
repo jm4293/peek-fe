c65b4c987c31862629cb99c5d968e3e981245478
jm4293 <dlwoals4293@gmail.com>
2025-10-01 14:30:11 +0900

    style: 불필요 코드 제거

--- app/(main)/user/User.tsx
+++ app/(main)/user/User.tsx
@@ -1,13 +1,7 @@
-'use client';
-
-import { validateHeaderName } from 'http';
 import { ChevronRight } from 'lucide-react';
 import Link from 'next/link';
-import { useState } from 'react';
 
 import { Thumbnail } from '@/components/image';
-import { Datalist } from '@/components/select';
-import Select from '@/components/select/select';
 import { Text } from '@/components/text';
 import { Wrapper } from '@/components/wrapper';
 
@@ -22,32 +16,8 @@ interface IProps {
 export default function User(props: IProps) {
   const { myInfo } = props;
 
-  const [datalistValue, setDatalistValue] = useState('');
-
   return (
     <Wrapper.SECTION>
-      <Select
-        title="언어"
-        name="language"
-        options={[
-          { value: 'korean', label: '한국어' },
-          { value: 'english', label: 'English' },
-          { value: 'japanese', label: '日本語' },
-        ]}
-      />
-
-      <Datalist
-        title="국가"
-        name="country"
-        options={[
-          { value: 'kr', label: '대한민국' },
-          { value: 'us', label: '미국' },
-          { value: 'jp', label: '일본' },
-        ]}
-        value={datalistValue}
-        onChange={(e) => setDatalistValue(e.target.value)}
-      />
-
       <Link href="/user/detail" className="py-1 flex items-center justify-between">
         <div className="w-full flex items-center gap-4">
           <Thumbnail thumbnail={myInfo.user.thumbnail} size={32} />
