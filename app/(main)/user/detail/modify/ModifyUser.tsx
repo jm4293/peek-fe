faf74f3ca65964c81faeb5e3bbe5e11b6cf39922
jm4293 <dlwoals4293@gmail.com>
2025-11-24 10:17:28 +0900

    refactor: 유저 정보 변경 react-hook-form 변경

--- app/(main)/user/detail/modify/ModifyUser.tsx
+++ app/(main)/user/detail/modify/ModifyUser.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
-import { FormEvent } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { Button } from '@/components/button';
 import { Input } from '@/components/input';
+import { Text } from '@/components/text';
 import { Wrapper } from '@/components/wrapper';
 
-import { useModal } from '@/hooks/modal';
-
 import { UserAccountModel, useUserMutation } from '@/services/user';
+import { UpdateUserInfoReq, updateUserInfoReqSchema } from '@/services/user/type';
 
 interface Props {
   userInfo: UserAccountModel;
@@ -18,44 +19,51 @@ interface Props {
 export default function ModifyUser(props: Props) {
   const { userInfo } = props;
 
-  const { openModal, closeModal } = useModal();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UpdateUserInfoReq>({
+    resolver: zodResolver(updateUserInfoReqSchema),
+    defaultValues: {
+      nickname: userInfo.user.nickname,
+    },
+  });
 
   const { updateUserMutation } = useUserMutation();
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
-    const nickname = formData.get('nickname');
-
-    if (!nickname) {
-      openModal({
-        title: '알림',
-        content: '닉네임을 입력해주세요.',
-        onConfirm: closeModal,
-      });
-      return;
-    }
-
-    updateUserMutation.mutate({
-      nickname: nickname as string,
-    });
+  const onSubmit = async (formData: UpdateUserInfoReq) => {
+    updateUserMutation.mutate({ ...formData });
   };
 
   return (
     <Wrapper.SECTION>
-      <form className="flex flex-col gap-16" onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-16" onSubmit={handleSubmit(onSubmit)}>
         <div className="flex flex-col gap-8">
-          <Input title="이메일" name="email" placeholder="이메일" defaultValue={userInfo.email} disabled />
-          <Input title="이름" name="name" placeholder="이름" defaultValue={userInfo.user.name} disabled />
-          <Input title="닉네임" name="nickname" placeholder="닉네임" defaultValue={userInfo.user.nickname} required />
+          <Input label="이메일" name="email" placeholder="이메일" defaultValue={userInfo.email} disabled />
+
+          <div className="flex flex-col gap-2">
+            <Input
+              label="이름"
+              name="name"
+              placeholder="이름"
+              defaultValue={userInfo.user.name}
+              disabled
+              helperText="이름은 변경할 수 없습니다."
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Input label="닉네임" placeholder="닉네임" {...register('nickname')} helperText="공개되는 정보입니다." />
+            {errors.nickname && <Text.PARAGRAPH text={errors.nickname.message} color="red" />}
+          </div>
         </div>
 
         <div className="grid grid-cols-2 gap-2">
           <Link href="/user/detail">
             <Button.OUTLINE text="뒤로가기" />
           </Link>
-          <Button.CONTAINER type="submit" text="변경하기" />
+          <Button.CONTAINER type="submit" text="변경하기" disabled={updateUserMutation.isPending} />
         </div>
       </form>
     </Wrapper.SECTION>
--- services/user/type/user.type.ts
+++ services/user/type/user.type.ts
@@ -1,11 +1,15 @@
-interface BaseUser {
-  nickname: string;
-  name?: string;
-  birthday?: string;
-  thumbnail?: string;
-}
+import { z } from 'zod';
 
-export interface UpdateUserInfoReq extends BaseUser {}
+const baseUserSchema = z.object({
+  nickname: z.string().min(1, '닉네임은 최소 1자 이상이어야 합니다.').max(100, '닉네임은 최대 100자 이하여야 합니다.'),
+  // name: z.string().min(1, '이름은 최소 1자 이상이어야 합니다.').max(100, '이름은 최대 100자 이하여야 합니다.'),
+  // birthday: z.string().optional(),
+  // thumbnail: z.string().optional(),
+});
+
+export const updateUserInfoReqSchema = baseUserSchema;
+
+export type UpdateUserInfoReq = z.infer<typeof updateUserInfoReqSchema>;
 
 export interface UpdateUserInfoRes {}
 
